9c78123d953f24873fdfb833277c067ebbd3355f
TheOld <the.old@gmail.com>
2012-11-10 23:47:02 +0100

    Adding information screens

--- js/game.js
+++ js/game.js
@@ -1,20 +1,55 @@
 var DEBUG=0;
 var SKIN="ComHazzle";
 var DEFAULT_GAME_TIME=60;
+var NUMBER_OF_ROUNDS = 3;
 var blockInput = false;
 
 Crafty.c("Game", {
     init: function() {
-        this.addComponent("KeyboardEvent, 2D, Mouse");
+        this.addComponent("KeyboardEvent, 2D, DOM, Mouse");
+        this.attr({x:0, y:0, w:100, h:100});
 
         this._status = Crafty.e("Status")._make(120, 0);
+        this.bind('KeyDown', function(e) {
+            if(e.key == Crafty.keys['SPACE']
+                || e.key == Crafty.keys['ENTER']) {
+                if (this._isWelcomeScreen()) {
+                    this._preStartRound();
+                    return;
+                }
+                if (this._isTaskScreen()) {
+                    this._startRound();
+                    return;
+                }
+                if (this._isGameOverScreen()) {
+                    this._start();
+                    return;
+                }
+            }
+        });
 
         this.bind('Click', function(e) {
             if (blockInput) {
                 return;
             }
+            if (this._isWelcomeScreen()) {
+                this._preStartRound();
+                return;
+            }
+            if (this._isTaskScreen()) {
+                this._startRound();
+                return;
+            }
+            if (this._isGameOverScreen()) {
+                this._start();
+                return;
+            }
             if (this._isRoundOver()) {
-                this._roundOver();
+                if(this._roundNumber < NUMBER_OF_ROUNDS) {
+                    this._preStartRound();
+                } else {
+                    this._gameOver(false);
+                }
                 return;
             }
             var cell = this._board._getCellByCoords(e.realX, e.realY);
@@ -29,17 +64,39 @@ Crafty.c("Game", {
             }
         });
     },
+    _isScreenDisplayed: function(screen) {
+        var screens = Crafty(screen);
+        if (screens.length > 0) {
+            return true;
+        }
+        return false;
+    },
+    _isWelcomeScreen: function() {
+        return this._isScreenDisplayed('WelcomeScreen');
+    },
+    _isGameOverScreen: function() {
+        return this._isScreenDisplayed('GameOverScreen');
+    },
+    _isTaskScreen: function() {
+        return this._isScreenDisplayed('TaskScreen');
+    },
     _start: function() {
+        this._roundNumber = 0;
         this._countdown = 0;
-        this._startRound();
+        Crafty.e("WelcomeScreen");
+    },
+    _preStartRound: function() {
+        this._roundNumber++;
+        this._clearAll();
+        this._countdown += DEFAULT_GAME_TIME;
+        blockInput = false;
+        Crafty.e("TaskScreen")._setTime(this._countdown);
     },
     _startRound: function() {
-        this._board = Crafty.e("Board")._make(100, 100, SKIN);
+        this._clearAll();
+        this._board = Crafty.e("Board")._make(this._w, this._h, SKIN);
         this._board._randomize();
-        this.attr({x:this._x, y:this._y, w:this._board._w, h:this._board._h});
-        blockInput = false;
 
-        this._countdown += DEFAULT_GAME_TIME;
         this._status._setTime(this._countdown < 0 ? 0 : this._countdown);
         setTimeout(this._doCountdown, 1000);
     },
@@ -48,25 +105,23 @@ Crafty.c("Game", {
         game._countdown--;
         game._status._setTime(game._countdown < 0 ? 0 : game._countdown);
         if (game._isGameOver()) {
-            game._gameOver();
+            game._gameOver(true);
         } else if (game._isRoundOver()) {
         } else {
             setTimeout(game._doCountdown, 1000);
         }
     },
-    _gameOver: function() {
-        //alert("Game is over...");
+    _gameOver: function(gameLost) {
+        var game = Crafty("Game");
+        Crafty.e("GameOverScreen")._setStatus(gameLost, game._countdown);
     },
     _isGameOver: function() {
         return this._countdown < 0;
     },
-    _roundOver: function() {
-        this._clearAll();
-        this._startRound();
-    },
     _isRoundOver: function() {
-        var empty = this._board._getCellsByType(CELL_TYPE_EMPTY);
-        return empty.length == this._board._getNumOfCells();
+        var board = Crafty("Board");
+        var empty = board._getCellsByType(CELL_TYPE_EMPTY);
+        return empty.length == board._getNumOfCells();
     },
     _checkFlipped: function() {
         var game = Crafty("Game");
@@ -91,7 +146,7 @@ Crafty.c("Game", {
     },
     _clearAll: function() {
         var game = Crafty("Game");
-        var elements = Crafty('Card, Board');
+        var elements = Crafty('Card, Board, Screen, ScreenText');
         for (var i=0; i<elements.length; i++) {
             if (elements[i] != game) {
                 Crafty(elements[i]).destroy();
---
+++ js/screens.js
@@ -0,0 +1,73 @@
+Crafty.c("ScreenText", {
+    _textSize: 10,
+    _textColor: '#FFFFFF',
+    init: function() {
+        this.addComponent("2D, DOM, Text");
+        this.textColor(this._textColor, 1);
+        var size = this._textSize + "px";
+        this.textFont({size: size, family: 'Arial'});
+        this.css({textAlign: 'center'});
+    }
+});
+
+Crafty.c("Screen", {
+    init: function() {
+        this.addComponent("2D, DOM, Color");
+        this.attr({w:100, h:100});
+        this.color('#000000');
+    },
+});
+
+Crafty.c("WelcomeScreen", {
+    init: function() {
+        this.addComponent("Screen");
+        var text = Crafty.e("ScreenText");
+        text.text("Welcome to").attr({y:this._h/2-text._textSize, w:this._w});
+        var text2 = Crafty.e("ScreenText");
+        text2.text("PaiPuzzle game").attr({y:this._h/2, w:this._w});
+        var text3 = Crafty.e("ScreenText");
+        text3.text("Click to continue").attr({y:this._h-2*text3._textSize, w:this._w});
+    }
+});
+
+Crafty.c("TaskScreen", {
+    init: function() {
+        this._time = 60;
+        this.addComponent("Screen");
+    },
+    _setTime: function(time) {
+        this._time = time;
+        this._update();
+    },
+    _update: function() {
+        this._textRule = Crafty.e("ScreenText");
+        this._textRule.text("Click cards to find a pair.").attr({y:this._textRule._textSize, w:this._w});
+
+        var rule = "Saved seconds adds to time limit."
+        this._textSave = Crafty.e("ScreenText");
+        this._textSave.text(rule).attr({y:this._h/2-this._textRule._textSize, w:this._w});
+
+        var limit = "You have " + this._time + " seconds";
+        this._textLimit = Crafty.e("ScreenText");
+        this._textLimit.text(limit).attr({y:this._h-3*this._textLimit._textSize, w:this._w});
+    }
+});
+
+Crafty.c("GameOverScreen", {
+    init: function() {
+        this.addComponent("Screen");
+    },
+    _setStatus: function(timeIsUp, time) {
+        if(timeIsUp) {
+            var text = Crafty.e("ScreenText");
+            text.text("Time is up.").attr({y:this._h/3-text._textSize, w:this._w});
+            var text = Crafty.e("ScreenText");
+            text.text("Game is over.").attr({y:this._h/2, w:this._w});
+        } else {
+            var text = Crafty.e("ScreenText");
+            text.text("Your score is " + time + ".").attr({y:this._h/3-text._textSize, w:this._w});
+            var text = Crafty.e("ScreenText");
+            text.text("Game is completed!").attr({y:this._h/2, w:this._w});
+        }
+    }
+});
