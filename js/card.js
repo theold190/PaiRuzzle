f37e6e146b7a474a75192a1a115260c8dbd9e465
TheOld <the.old@gmail.com>
2012-11-10 19:38:45 +0100

    Using sprites for front parts

--- js/card.js
+++ js/card.js
@@ -2,24 +2,6 @@ var CELL_TYPE_EMPTY = 0;
 var CELL_TYPE_COVER = 1;
 var CELL_TYPE_FRONT = 2;
 
-function getColorByIndex(index)
-{
-    switch(index) {
-        case 0: return "#B2DB11";
-        case 1: return "#FFFF00";
-        case 2: return "#FF0000";
-        case 3: return "#00A0C6";
-        case 4: return "#66B821";
-        case 5: return "#FFB200";
-        case 6: return "#F0027F";
-        case 7: return "#0A50A1";
-        case 8: return "#008837";
-        case 9: return "#FF6600";
-        case 10: return "#81017E";
-        case 11: return "#13007C";
-    }
-}
-
 Crafty.c("Card", {
     init: function() {
         this.addComponent("2D, DOM, Color");
@@ -40,15 +22,24 @@ Crafty.c("Card", {
         this._type = type;
         switch(type) {
             case CELL_TYPE_EMPTY:
-                this.color("#FF00FF");
                 this.alpha=0.0;
                 break;
             case CELL_TYPE_COVER:
-                this.color("#C1C1C1");
+                if(this.has(this._frontSprite)) {
+                    this.removeComponent(this._frontSprite, false);
+                }
+                if(!this.has(this._coverSprite)) {
+                    this.addComponent(this._coverSprite);
+                }
                 this.alpha=1.0;
                 break;
             case CELL_TYPE_FRONT:
-                this.color(getColorByIndex(this._index));
+                if(this.has(this._coverSprite)) {
+                    this.removeComponent(this._coverSprite, false);
+                }
+                if(!this.has(this._frontSprite)) {
+                    this.addComponent(this._frontSprite);
+                }
                 this.alpha=1.0;
                 break;
         }
@@ -81,6 +72,8 @@ Crafty.c("Card", {
     },
     _setIndex: function(index) {
         this._index = index;
+        this._frontSprite = "sprite_front"+index;
+        this._coverSprite = "sprite_cover";
         this._update(CELL_TYPE_COVER);
     },
     _isSameIndex: function(cell) {
--- js/sprites.js
+++ js/sprites.js
@@ -1,3 +1,32 @@
-Crafty.sprite(80, "img/background.png", {
+Crafty.sprite(100, "img/background.png", {
     sprite_background: [0, 0]
+});
+
+Crafty.sprite(25, "img/cover.png", {
+    sprite_cover: [0, 0]
+});
+
+Crafty.sprite(25, "img/front0.png", {
+    sprite_front0: [0, 0]
+});
+Crafty.sprite(25, "img/front1.png", {
+    sprite_front1: [0, 0]
+});
+Crafty.sprite(25, "img/front2.png", {
+    sprite_front2: [0, 0]
+});
+Crafty.sprite(25, "img/front3.png", {
+    sprite_front3: [0, 0]
+});
+Crafty.sprite(25, "img/front4.png", {
+    sprite_front4: [0, 0]
+});
+Crafty.sprite(25, "img/front5.png", {
+    sprite_front5: [0, 0]
+});
+Crafty.sprite(25, "img/front6.png", {
+    sprite_front6: [0, 0]
+});
+Crafty.sprite(25, "img/front7.png", {
+    sprite_front7: [0, 0]
 });
